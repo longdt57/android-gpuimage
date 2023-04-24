c7067732ba4200c49bab8535c7252848f08538d9
Long Do <Longdt57@gmail.com>
2023-04-25 00:45:33 +0900

    Fix bug black background

--- library/src/main/java/jp/co/cyberagent/android/gpuimage/GPUImageRenderer.java
+++ library/src/main/java/jp/co/cyberagent/android/gpuimage/GPUImageRenderer.java
@@ -117,6 +117,7 @@ public class GPUImageRenderer implements GLSurfaceView.Renderer, GLTextureView.R
 
     @Override
     public void onDrawFrame(final GL10 gl) {
+        GLES20.glClearColor(backgroundRed, backgroundGreen, backgroundBlue, 1);
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
         runAll(runOnDraw);
         filter.onDraw(glTextureId, glCubeBuffer, glTextureBuffer);
